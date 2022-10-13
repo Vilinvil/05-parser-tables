fa795ef19dba9c735a3976243c1ee60baf02c552
Vladislav Ilinskiy <ivn15072002@gmail.com>
2022-10-13 20:05:11 +0300

    add ScannerFromFile

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package vilin_parser
 
+import (
+	"bufio"
+	"fmt"
+	"golang.org/x/text/encoding/charmap"
+	"golang.org/x/text/transform"
+	"os"
+)
+
+func ScannerFromFile(filePath string) (scanner *bufio.Scanner, err error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("can`t open file in ScannerFromFile. Error is: %w", err)
+	}
+	defer func() {
+		err = file.Close()
+	}()
+
+	// Use charmap.ISO8859_1.NewDecoder() because the data contains characters in the encoding of the ISO8859_1
+	reader := transform.NewReader(file, charmap.ISO8859_1.NewDecoder())
+	scanner = bufio.NewScanner(reader)
+
+	return scanner, nil
+}
+
 func main() {
 
 }
